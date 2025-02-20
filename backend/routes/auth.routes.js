1f99fbc0d7849ca62eddce5f9b51082be396db64
LGarza94 <161745045+LGarza94@users.noreply.github.com>
2025-02-19 20:12:33 -0600

    added endpoints for specific routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,10 +2,24 @@ import express from "express"
 
 const router = express.Router();
 
-router.get("/signup", (req,res) => {
+router.post("/signup", (req,res) => {
     res.json({
         data: "You hit the signup endpoint",
     });
 });
+
+router.post("/login", (req,res) => {
+    res.json({
+        data: "You hit the login endpoint",
+    });
+});
+
+router.post("/logout", (req,res) => {
+    res.json({
+        data: "You hit the logout endpoint",
+    });
+});
+
+
 
 export default router;

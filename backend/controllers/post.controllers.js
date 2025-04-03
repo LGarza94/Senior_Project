9c4e94f0a5413db661045000f9d85681cedfaa04
LGarza94 <161745045+LGarza94@users.noreply.github.com>
2025-04-03 14:22:26 -0500

    file name changes into routes folder, files changed from routes to route

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -241,5 +241,5 @@ export const getUserPosts = async(req, res) => {
         console.log("Error in getUserPosts controller: ", error);
         res.status(500).json({error: "Internal server error"});
     }
-}
+};
 

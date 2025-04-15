5222dfaa2ba98daf6cacece82bea1df9bc50e086
LGarza94 <161745045+LGarza94@users.noreply.github.com>
2025-04-15 00:56:04 -0500

    Added functionality to create posts within the app and have the application update the frontend

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,11 +2,15 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {toast} from 'react-hot-toast';
 
 const CreatePost = ({currentTab = "forYou"}) => {
 	const [text, setText] = useState("");
 	const [img, setImg] = useState(null);
-  //const [vid, setVid] = useState(null);
+	//const [vid, setVid] = useState(null);
+	const imgRef = useRef(null);
+	//const vidRef = useRef(null);
   
 	const getPostTypeFromTab = (tab) => {
 		switch(tab){
@@ -18,13 +22,40 @@ const CreatePost = ({currentTab = "forYou"}) => {
 		}
 	}
 
+	const {data:authUser} = useQuery({queryKey: ['authUser']});
+	const queryClient = useQueryClient();
+
+	const {mutate:createPost, isPending, isError, error} = useMutation({
+		mutationFn: async ({text, img}) => {
+			try{
+				const res = await fetch("/api/posts/create", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({text,img}),
+				})
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			}catch(error){
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			setText("");
+			setImg(null);
+			//setVid(null);
+			toast.success("Post created successfully");
+			queryClient.invalidateQueries({queryKey: ['posts']});
+		},
+	})
+
 	const postType = getPostTypeFromTab(currentTab);
 	//const [postType, setPostType] = useState("regular");
 
-	const imgRef = useRef(null);
-  	//const vidRef = useRef(null);
-	const isPending = false;
-	const isError = false;
 
 	const data = {
 		profileImg: "/avatars/boy1.png",
@@ -32,6 +63,7 @@ const CreatePost = ({currentTab = "forYou"}) => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		createPost({text, img});
 
 		console.log("Creating post with type:", postType);
 		alert("Post created successfully");
@@ -52,7 +84,7 @@ const CreatePost = ({currentTab = "forYou"}) => {
 		<div className='flex p-4 items-start gap-4 border-b border-gray-700'>
 			<div className='avatar'>
 				<div className='w-8 rounded-full'>
-					<img src={data.profileImg || "/avatar-placeholder.png"} />
+					<img src={authUser.profileImg || "/avatar-placeholder.png"} />
 				</div>
 			</div>
 			<form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
@@ -90,7 +122,7 @@ const CreatePost = ({currentTab = "forYou"}) => {
 						{isPending ? "Posting..." : "Post"}
 					</button>
 				</div>
-				{isError && <div className='text-red-500'>Something went wrong</div>}
+				{isError && <div className='text-red-500'>{error.message}</div>}
 			</form>
 		</div>
 	);
